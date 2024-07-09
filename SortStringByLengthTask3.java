7912e4853ef25e57106545e1aed47831df2f8c9c
developersmita767 <156692415+developersmita767@users.noreply.github.com>
2024-07-09 23:56:11 +0530

    Add files via upload

---
+++ SortStringByLengthTask3.java
@@ -0,0 +1,21 @@
+//Write program to sort list of strings by the length of the strings
+package Java8_Task;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortStringByLengthTask3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> stringList=new ArrayList<>();
+		stringList.add("apple");
+		stringList.add("orange");
+		stringList.add("pineapple");
+		stringList.add("grapes");
+		stringList.add("anar");
+//		Collections.length(stringList);
+
+	}
+
+}
